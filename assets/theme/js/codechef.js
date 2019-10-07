fab0041eb428fc15cb89abc32d241a32eba6ca7e
imdhruvit <iamdhruvit@gmail.com>
2019-10-07 19:28:00 +0530

    made with love

---
+++ assets/theme/js/codechef.js
@@ -0,0 +1,1 @@
+$('#m').load('https://www.codechef.com/ratings/all?filterBy=Country%3DIndia%3BInstitution%3DG%20H%20Patel%20College%20of%20Engineering%20and%20Technology&order=asc&sortBy=global_rank .table');
